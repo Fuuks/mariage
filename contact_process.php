eb9c474c2f332714d5795369ad37cd7ba4e492b7
Fuuks <fukiwony@gmail.com>
2018-08-27 23:54:23 +0200

    update form contact

--- contact_process.php
+++ contact_process.php
@@ -1,7 +1,7 @@
 <?php
 
 
-	echo "toto";
+
 
      // Plusieurs destinataires
      $to  = 'fukiwony@gmail.com'; // notez la virgule
@@ -10,34 +10,14 @@
      $subject = 'Calendrier des anniversaires pour Août';
 
      // message
-     $message = '
-     <html>
-      <head>
-       <title>Calendrier des anniversaires pour Août</title>
-      </head>
-      <body>
-       <p>Voici les anniversaires à venir au mois d\'Août !</p>
-       <table>
-        <tr>
-         <th>Personne</th><th>Jour</th><th>Mois</th><th>Année</th>
-        </tr>
-        <tr>
-         <td>Josiane</td><td>3</td><td>Août</td><td>1970</td>
-        </tr>
-        <tr>
-         <td>Emma</td><td>26</td><td>Août</td><td>1973</td>
-        </tr>
-       </table>
-      </body>
-     </html>
-     ';
+     $message = 'Bonjour je mapelle fuki';
 
      // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
      $headers[] = 'MIME-Version: 1.0';
      $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
      
-
+     
      // Envoi
      mail($to, $subject, $message, implode("\r\n", $headers));
 
