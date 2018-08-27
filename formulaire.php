29b17967a4a10f8f85eb7bef3ef85430a1fd17bf
Fuuks <fukiwony@gmail.com>
2018-08-27 23:50:56 +0200

    update form contact

---
+++ formulaire.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    $to = "fukiwony@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+
+    echo $to;
+  
+?>
