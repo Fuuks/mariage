e432f0556f76c61dbc82212d3e43867c41d815be
Fuuks <fukiwony@gmail.com>
2018-09-02 19:34:05 +0200

    test conn

--- formulaire.php
+++ formulaire.php
@@ -18,7 +18,12 @@
     $portable =  $_POST['portable'];
     $present = $_POST['present'];
     $horaire = $_POST['horaire'];
-    $nb_personnes = $_POST['nb_personnes'];
+	$nb_personnes = $_POST['nb_personnes'];
+	
+	$req = $conn->prepare('INSERT INTO inscription VALUES (?, ?, ?, ?, ?,?)');
+
+	$req->execute(array($nom,$prenom,$portable,$present,$horaire,$nb_personnes));
+	header('Location:index.html');
 
 /*
 	echo $nom;
