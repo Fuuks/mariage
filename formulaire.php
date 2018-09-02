d45d9f0a83ca3163fc12015d0829763a55e86768
Fuuks <fukiwony@gmail.com>
2018-09-02 12:15:13 +0200

    test

--- formulaire.php
+++ formulaire.php
@@ -26,7 +26,7 @@
 	echo $prenom;
 
 	 if($present == "oui"){
-		$sql ="INSERT INTO inscription(nom, prenom, portable, present, horaire, nb_personnes) VALUES('papa','test','065948394','oui,'19h','4')";
+		$sql ='INSERT INTO inscription(nom, prenom, portable, present, horaire, nb_personnes) VALUES("papa,"test","065948394","oui","19h",4)';
 		$conn->exec($sql);
     }else{
 		$conn->exec('INSERT INTO inscription(nom, prenom, portable, present,) VALUES($nom,$prenom,$portable,$present)');
