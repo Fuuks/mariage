394396efba3382244dcf4be7f878748b55059aa2
Fuuks <fukiwony@gmail.com>
2018-09-02 11:43:29 +0200

    form presence

--- formulaire.php
+++ formulaire.php
@@ -1,19 +1,19 @@
 <?php
 
 
-	/*try{
-		$host ="mysql:host=localhost";
-		$user = votre_login;
-		$bdd = Nom_de_la_base_de_donnees;
-		$passwd = Mot_de_passe;
+	try{
+		$host ="mysql:host=db752003822.db.1and1.com";
+		$user = "dbo752003822";
+		$bdd = "db752003822";
+		$passwd = "Jpgatien1993?";
 
-	//	$bdd = new PDO('$host;dbname=test;charset=utf8',$user,$passwd);
+		$bdd = new PDO('$host;dbname=$bdd;charset=utf8',$user,$passwd);
 	}
 	catch (Exception $e)
 	{
 
-	//	die('Erreur : ' . $e->getMessage());
-	}*/
+		die('Erreur : ' . $e->getMessage());
+	}
 
 	$nom = $_POST['nom'];
 	$prenom = $_POST['prenom'];
@@ -23,17 +23,20 @@
     $nb_personnes = $_POST['nb_personnes'];
 
 
-//    if(){
+	echo $nom;
+	echo $prenom;
+/*
+	 if(){
     	//$bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES());
 
-  //  }else{
+    }else{
 		//$bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES());    	
-    //}
+    }
     
-	// On ajoute une entrée dans la table jeux_video
-	//$bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES());
+	
+	$bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES());
 
-
+ */
     header('Location:formulaire-presence.php?present='.$present); 
   
 ?>
