882f393d8f22376fc3188a00eb924a4a26b0962d
Fuuks <fukiwony@gmail.com>
2025-02-11 22:08:25 +0100

    feat(): update txt #2

--- contact.php
+++ contact.php
@@ -143,7 +143,7 @@
                         <div class="col-lg-6">
                             <div class="contact_text">
                                 <div class="main_title">
-                                    <h2>Contactez nous</h2>
+                                    <h2>Contactez-nous</h2>
                                     <p>En cas de question concernant l'organisation du mariage, n'hésitez pas à nous contacter par mail.</p>
                                 </div>
                                 <div class="contact_d_list">
