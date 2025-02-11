08de94bd9cd68e90ad930d63955bcca86ba2168d
Fuuks <fukiwony@gmail.com>
2025-02-11 22:55:25 +0100

    feat(): list admin

---
+++ my_orga.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Mariage Cris & Christy</title>
+
+        <!-- Icon css link -->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/giga-sans" rel="stylesheet">
+        <link href="vendors/elegant-icon/style.css" rel="stylesheet">
+        <link href="vendors/themify-icon/themify-icons.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Rev slider css -->
+        <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="vendors/animate-css/animate.css" rel="stylesheet">
+
+        <!-- Extra plugin css -->
+        <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <!--================Search Area =================-->
+        <section class="search_area">
+            <div class="search_inner">
+                <input type="text" placeholder="Enter Your Search...">
+                <i class="ti-close"></i>
+            </div>
+        </section>
+        <!--================End Search Area =================-->
+
+        <!--================Header Menu Area =================-->
+        <header class="main_menu_area">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand font-italic menu_logo"  href="index.html">Cris & Christy<br>28 Mars 2025</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item "><a class="nav-link" href="index.html">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="programmation-journee.html">Programmation de la journée</a></li>
+                        <li class="nav-item"><a class="nav-link" href="formulaire-presence.php">Formulaire de présence</a></li>
+                        <li class="nav-item"><a class="nav-link" href="informations-pratiques.html">Infos Pratiques</a></li>
+                        <li class="nav-item active"><a class="nav-link" href="contact.php">Contacts</a></li>
+                    </ul>
+                    
+                </div>
+            </nav>
+        </header>
+        <!--================End Header Menu Area =================-->
+
+        <!--================Banner Area =================-->
+        <section class="banner_area">
+            <div class="container">
+                <div class="banner_text_inner">
+                    <h4>Contactez nous</h4>
+                    <h5>Si vous avez des questions sur l'organisation contactez nous</h5>
+                    <br>
+                    <ul>
+                        <li><a href="index.html"><i class="fa fa-home" aria-hidden="true"></i>Accueil</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Contacts</a></li>
+                        <li><a href="#"><i class="fa fa-address-book" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </section>
+        <!--================End Banner Area =================-->
+
+        <!--================Contact Us Area =================-->
+        <section class="contact_us_area">
+            
+        <h1>Liste des Personnes</h1>
+
+        <?php
+
+
+try {
+    $host ="mysql-cris-christy-milalu.alwaysdata.net;dbname=cris-christy-milalu_test";
+    $user = "396001_user";
+    
+    $passwd = "";
+
+    $conn = new PDO($host, $user, $passwd);
+    $conn->exec('SET NAMES utf8');
+} catch ( PDOException $e ) {
+    die( 'Connection failed: ' . $e->getMessage() );
+}
+
+
+$req = $conn->prepare("SELECT nom, prenom, portable, presence, horaire, nombre_personnes FROM inscription";);
+$req->execute();
+
+// Récupération des résultats sous forme de tableau associatif
+$resultats = $$req->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Portable</th>
+            <th>Présence</th>
+            <th>Horaire</th>
+            <th>Nombre de Personnes</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($resultats as $personne): ?>
+            <tr>
+                <td><?= htmlspecialchars($personne["nom"]) ?></td>
+                <td><?= htmlspecialchars($personne["prenom"]) ?></td>
+                <td><?= htmlspecialchars($personne["portable"]) ?></td>
+                <td><?= htmlspecialchars($personne["presence"]) ?></td>
+                <td><?= htmlspecialchars($personne["horaire"]) ?></td>
+                <td><?= htmlspecialchars($personne["nb_personnes"]) ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+        </section>
+        <!--================End Contact Us Area =================-->
+        <!--================Footer Area =================-->
+        <footer class="footer_area">
+            <div class="footer_widgets_area">
+                <div class="container">
+                    <div class="f_widgets_inner row">
+                        <div class="col-lg-6 col-md-12">
+                            <aside class="f_widget categories_widget">
+                                <div class="f_w_title">
+                                    <h3>Liens des Categories</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="index.html"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Accueil</a></li>
+                                    <li><a href="programmation-journee.html"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Programmation de la journée</a></li>
+                                    <!--<li><a href="#"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Carte d'invitation</a></li>-->
+                                    <li><a href="formulaire-presence.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Formulaire de présence</a></</li>
+                                    <li><a href="informations-pratiques.html"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Infos Pratiques</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <aside class="f_widget contact_widget">
+                                <div class="f_w_title">
+                                    <h3>Contactez-nous</h3>
+                                </div>
+
+                               <a href="mailto:crismilalu@gmail.com">crismilalu@gmail.com</a>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copy_right_area">
+                <div class="container">
+                    <div class="float-md-left">
+                        <h5>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib & update by W.F</a></h5>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--================End Footer Area =================-->
+        <!--================Contact Success and Error message Area =================-->
+        <div id="success" class="modal modal-message fade" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <i class="fa fa-close"></i>
+                        </button>
+                        <h2>Thank you</h2>
+                        <p>Your message is successfully sent...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modals error -->
+
+        <div id="error" class="modal modal-message fade" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <i class="fa fa-close"></i>
+                        </button>
+                        <h2>Sorry !</h2>
+                        <p> Something went wrong </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--================End Contact Success and Error message Area =================-->
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <!-- Extra plugin css -->
+        <script src="vendors/counterup/jquery.waypoints.min.js"></script>
+        <script src="vendors/counterup/jquery.counterup.min.js"></script>
+        <script src="vendors/counterup/apear.js"></script>
+        <script src="vendors/counterup/countto.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/parallaxer/jquery.parallax-1.1.3.js"></script>
+        <!--Tweets-->
+        <script src="vendors/tweet/tweetie.min.js"></script>
+        <script src="vendors/tweet/script.js"></script>
+        <!--gmaps Js-->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+        <script src="js/gmaps.min.js"></script>
+
+        <!-- contact js -->
+        <script src="js/contact.js"></script>
+       
+
+        <script src="js/theme.js"></script>
+    </body>
+</html>
