f5686ffa70cfe34babbe62dae698f0c53a430ff8
Fuuks <fukiwony@gmail.com>
2025-02-12 01:21:54 +0100

    feat(): css finish #3

--- formulaire-presence.php
+++ formulaire-presence.php
@@ -208,7 +208,7 @@
 								  </fieldset>
 
                                   <div class="form-group row" id="nb_people">
-                                      <label for="example-number-input" class="col-2 col-form-label">Nombre de personnes <strong>(même groupe/famille)</strong></label>
+                                      <label for="example-number-input" class="col-2 col-form-label">Nombre de personnes accompagnants <strong>(même groupe/famille)</strong></label>
                                       <div class="col-10">
                                         <input class="form-control" name="nb_personnes" type="number" min="0" id="example-number-input">
                                       </div>
